19da726ce279c8e12813efdf4748906a8d029abc
pipe-dev <pipe-dev>
2022-07-02 20:05:33 -0500

    feature: interface vs types

---
+++ interface_vs_types_01/functions.ts
@@ -0,0 +1,14 @@
+
+interface sayHiToIsma {
+    (name: string): string;
+  }
+  
+  type sayHiToJavi = (name: string) => string;
+  
+  const sayHiToIsma: sayHiToIsma = (name: string) => {
+    return `Hello Isma, my name is ${name}`;
+  };
+  
+  const sayHiToJavi: sayHiToJavi = function (name) {
+    return `Hello Javi, my name is ${name}`;
+  };
